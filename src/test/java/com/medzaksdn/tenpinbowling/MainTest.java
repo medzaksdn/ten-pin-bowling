079447c237742c2eb2200af76451731745a295e2
Mohamed-Zakaria SAIDANE <mohamedzakariasaidane@gmail.com>
2021-03-18 14:56:10 +0100

    add tests

--- src/test/java/com/medzaksdn/tenpinbowling/MainTest.java
+++ src/test/java/com/medzaksdn/tenpinbowling/MainTest.java
@@ -1,4 +1,53 @@
 package com.medzaksdn.tenpinbowling;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Mohamed-Zakaria SAIDANE
+ */
 public class MainTest {
+
+    @Test
+    public void testValidSequenceAndTotalScore() {
+        //Given
+        String sequence = "XXXXXXXXXXXX";
+
+        //When
+        Game game = new Game();
+        game.setSequence(sequence);
+        int totalScore = game.printScoreSheetAndReturnTotal();
+
+        //Then
+        assertEquals(totalScore, 300);
+    }
+
+    @Test
+    public void testInvalidCharacter() {
+        //Given
+        String sequence = "Invalid character";
+
+        //When
+        Game game = new Game();
+        IllegalArgumentException e =assertThrows(IllegalArgumentException.class, () -> {game.setSequence(sequence);});
+
+        //Then
+        assertEquals(e.getMessage(), "There is an invalid character in the sequence");
+    }
+
+    @Test
+    public void testImpossibleSpare() {
+        //Given
+        String sequence = "/2";
+
+        //When
+        Game game = new Game();
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {game.setSequence(sequence);});
+
+        //Then
+        assertEquals(e.getMessage(), "There is an impossible spare in the sequence");
+    }
+
 }
