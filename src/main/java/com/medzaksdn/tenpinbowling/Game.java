fc2ba59dc4dd71ef753811f338c1cbce5972cb3d
Mohamed-Zakaria SAIDANE <mohamedzakariasaidane@gmail.com>
2021-03-18 06:04:22 +0100

    WIP

--- src/main/java/com/medzaksdn/tenpinbowling/Game.java
+++ src/main/java/com/medzaksdn/tenpinbowling/Game.java
@@ -9,12 +9,11 @@ import java.util.Map;
  * @author Mohamed-Zakaria SAIDANE
  */
 public class Game {
-
-    private int numberOfFrames=0;
     private Map<Integer, List<BowlingThrow>> bowlingFrames;
 
     public void setSequence(final String  sequence) {
         bowlingFrames = new HashMap<>();
+        int numberOfFrames=0;
         boolean frameEnd = false;
         List<BowlingThrow> bowlingThrows = new ArrayList<>();
         for(int i=0; i<sequence.length(); i++) {
@@ -22,69 +21,81 @@ public class Game {
             if(bowlingThrow.isSpare() && bowlingThrows.size() == 0)
                 throw new IllegalArgumentException("There is an impossible spare in the sequence");
 
-            if(bowlingThrows.size() == 1)
+            if(bowlingThrows.size()>0)
                 bowlingThrow.setPreviousBowlingThrow(bowlingThrows.get(0));
 
             bowlingThrows.add(bowlingThrow);
 
-            if(bowlingThrow.isStrike())
-                frameEnd = true;
+            if(numberOfFrames<9) {
+                if (bowlingThrow.isStrike())
+                    frameEnd = true;
 
-            if(frameEnd) {
-                this.bowlingFrames.put(this.numberOfFrames, bowlingThrows);
-                bowlingThrows = new ArrayList<>();
-                this.numberOfFrames++;
+                if (frameEnd) {
+                    this.bowlingFrames.put(numberOfFrames, bowlingThrows);
+                    bowlingThrows = new ArrayList<>();
+                    numberOfFrames++;
+                }
+
+                frameEnd = !frameEnd;
             }
-
-            frameEnd = !frameEnd;
+            else if(i == sequence.length()-1)
+                this.bowlingFrames.put(numberOfFrames, bowlingThrows);
         }
     }
 
     public void printScoreSheet() {
+        System.out.println(this.bowlingFrames.size());
         System.out.print("Frame :         ");
-        for(int i=0; i<this.numberOfFrames; i++) {
+        for(int i=0; i<this.bowlingFrames.size(); i++) {
             System.out.print(i+1 +"     ");
         }
         System.out.println();
 
         System.out.print("Pins knocked : |");
-        for(int i=0; i<this.numberOfFrames; i++) {
+        for(int i=0; i<this.bowlingFrames.size(); i++) {
             for(BowlingThrow bowlingThrow : this.bowlingFrames.get(i)) {
-                if(bowlingThrow.isStrike())
-                    System.out.print(bowlingThrow.getPin()+"| |");
-                else
-                    System.out.print(bowlingThrow.getPin()+"|");
+                if (bowlingThrow.isStrike()) {
+                    if (i < 9)
+                        System.out.print(bowlingThrow.getPin() + "| |");
+                    else
+                        System.out.print(bowlingThrow.getPin() + "|");
+                } else
+                    System.out.print(bowlingThrow.getPin() + "|");
             }
-            System.out.print(" |");
+            if(i!=this.bowlingFrames.size()-1)
+                System.out.print(" |");
         }
         System.out.println();
 
         System.out.print("Score :         ");
         int frameScore;
         int totalScore = 0;
-        List<BowlingThrow> bowlingFrames;
-        for(int i=0; i<this.numberOfFrames; i++) {
+        List<BowlingThrow> bowlingFrame;
+        for(int i=0; i<this.bowlingFrames.size(); i++) {
             frameScore = 0;
-            bowlingFrames = this.bowlingFrames.get(i);
-            if(bowlingFrames.size() == 2) {
-                frameScore = bowlingFrames.get(0).getNumberOfPinsKnocked() + bowlingFrames.get(1).getNumberOfPinsKnocked();
-                if(i<9 && bowlingFrames.get(1).isSpare())
+            bowlingFrame = this.bowlingFrames.get(i);
+            if(bowlingFrame.size() > 1) {
+                frameScore = bowlingFrame.stream().mapToInt(bowlingThrow->bowlingThrow.getNumberOfPinsKnocked()).sum();
+                if(i<9 && bowlingFrame.get(1).isSpare())
                     frameScore += this.bowlingFrames.get(i+1).get(0).getNumberOfPinsKnocked();
             }
-            else if(bowlingFrames.get(0).isStrike()) {
-                frameScore = bowlingFrames.get(0).getNumberOfPinsKnocked();
+            else if(bowlingFrame.get(0).isStrike()) {
                 if(i<9) {
-                    if(this.bowlingFrames.get(i+1).size() == 2)
+                    frameScore = bowlingFrame.get(0).getNumberOfPinsKnocked();
+                    if(this.bowlingFrames.get(i+1).size() > 1)
                         frameScore+= this.bowlingFrames.get(i+1).get(0).getNumberOfPinsKnocked() + this.bowlingFrames.get(i+1).get(1).getNumberOfPinsKnocked();
                     else
                         frameScore += this.bowlingFrames.get(i+1).get(0).getNumberOfPinsKnocked() + this.bowlingFrames.get(i+2).get(0).getNumberOfPinsKnocked();
+                }
+                else {
+                    frameScore = bowlingFrame.stream().mapToInt(bowlingThrow->bowlingThrow.getNumberOfPinsKnocked()).sum();
                 }
             }
             totalScore+= frameScore;
             System.out.print("|"+totalScore+"|  ");
         }
         System.out.println();
-        System.out.print("Total score : "+ totalScore);
+        System.out.println("Total score : "+ totalScore);
     }
 
 }
